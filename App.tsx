a848eae33cb6cc68b17cf2cc7931b1511db43658
baolinhdev <baolinhdev@gmail.com>
2024-09-17 20:48:57 +0700

    solve with numeric values

--- App.tsx
+++ App.tsx
@@ -1,16 +1,49 @@
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  const [count, setCount] = useState<number>(0)
+  const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number>(0);
 
   return (
     <View style={styles.container}>
-      <Text style = {{ fontSize: 40, fontWeight: "600"}}>
-        count: {count}
-      </Text>
-      <View> 
-        <Button color={"red"} title="Increase" onPress={() => setCount(count + 1)} />
+      <View>
+        <Text style={{ fontSize: 40, fontWeight: "600" }}>Name: {name}</Text>
+        <TextInput
+          multiline
+          onChangeText={(value) => setName(value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15,
+          }}
+        />
+      </View>
+      <View>
+        <Text style={{ fontSize: 40, fontWeight: "600" }}>Age: {age}</Text>
+        <TextInput
+          multiline
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15,
+          }} 
+          keyboardType="numeric" 
+          maxLength={2}
+          onChangeText={(value) => setAge(+value)}
+        />
+      </View>
+
+      <Text style={{ fontSize: 40, fontWeight: "600" }}>count: {count}</Text>
+      <View>
+        <Button
+          color={"red"}
+          title="Increase"
+          onPress={() => setCount(count + 1)}
+        />
       </View>
     </View>
   );
